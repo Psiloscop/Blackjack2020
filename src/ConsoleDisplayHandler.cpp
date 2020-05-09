5b5631145df523e48cf97c5d400ab079a7d064c3
Sergey Filipovich <psiloscop@gmail.com>
2020-05-09 21:14:24 +0600

    Fixed all issues with console!!!

--- src/ConsoleDisplayHandler.cpp
+++ src/ConsoleDisplayHandler.cpp
@@ -2,7 +2,11 @@
 
 void ConsoleDisplayHandler::clearConsole() const
 {
-//    std::cout << std::string(50, '\n');
+#if defined(_WIN32) || defined(_WIN64)
+    system("cls");
+#else
+    system("clear");
+#endif
 }
 
 void ConsoleDisplayHandler::display(ConsoleDisplayEntity* entity) const
--- src/main.cpp
+++ src/main.cpp
@@ -20,53 +20,9 @@ void initConsoleApplication()
     app.createPlayer();
 }
 
-
 int main()
 {
     initConsoleApplication();
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-//#include <iostream>
-//#include <algorithm>
-//#include <windows.h>
-//
-//void clear_screen(char fill = ' ') {
-//    COORD tl = {0,0};
-//    CONSOLE_SCREEN_BUFFER_INFO s;
-//    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-//
-//    GetConsoleScreenBufferInfo(console, &s);
-//
-//    DWORD written, cells = s.dwSize.X * s.dwSize.Y;
-//
-//    FillConsoleOutputCharacter(console, fill, cells, tl, &written);
-//    FillConsoleOutputAttribute(console, s.wAttributes, cells, tl, &written);
-//    SetConsoleCursorPosition(console, tl);
-//}
-//
-//int main()
-//{
-//    std::cout << "test1" << std::endl;
-////    std::cout << "test2";
-////    std::cout << "test3";
-//
-////    std::string string;
-////    std::getline(std::cin, string);
-////    string.erase(remove(string.begin(), string.end(), '\n'), string.end());
-//
-//    clear_screen();
-//
-//    std::cout << "test2";
-//
-//    return 0;
-//}
